02c93a2ef14b3a57cf12ecb250673107a49ba596
안재철 <qnzo8888@gmail.com>
2019-08-24 20:22:54 +0900

    필드수정

--- sql/test.sql
+++ sql/test.sql
@@ -23,9 +23,9 @@ insert into bdcards (serial_number, blood_date, blood_dona_type, blood_bank_name
 insert into bdcards (serial_number, blood_date, blood_dona_type, blood_bank_name, reg_date, owner_id)values(20, CURRENT_TIMESTAMP(), '1', '1', '2000-01-01', '1');
 
 
-insert into reqboards (id, diagnosis, title, need_count, story, req_date, used_place, req_user_id)values(1, null, '1', 10, '1', CURRENT_TIMESTAMP(), '1', 2);
-insert into reqboards (id, diagnosis, title, need_count, story, req_date, used_place, req_user_id)values(2, null, '1', 2, '1', CURRENT_TIMESTAMP(), '1', 2);
-insert into reqboards (id, diagnosis, title, need_count, story, req_date, used_place, req_user_id)values(3, null, '1', 1, '1', CURRENT_TIMESTAMP(), '1', 2);
-insert into reqboards (id, diagnosis, title, need_count, story, req_date, used_place, req_user_id)values(4, null, '1', 100, '1', CURRENT_TIMESTAMP(), '1', 2);
-insert into reqboards (id, diagnosis, title, need_count, story, req_date, used_place, req_user_id)values(5, null, '1', 50, '1', CURRENT_TIMESTAMP(), '1', 2);
-insert into reqboards (id, diagnosis, title, need_count, story, req_date, used_place, req_user_id)values(6, null, '1', 15, '1', CURRENT_TIMESTAMP(), '1', 2);
+insert into reqboards (id, diagnosis, title, need_count, story, reg_date, used_place, req_user_id)values(1, null, '1', 10, '1', CURRENT_TIMESTAMP(), '1', 2);
+insert into reqboards (id, diagnosis, title, need_count, story, reg_date, used_place, req_user_id)values(2, null, '1', 2, '1', CURRENT_TIMESTAMP(), '1', 2);
+insert into reqboards (id, diagnosis, title, need_count, story, reg_date, used_place, req_user_id)values(3, null, '1', 1, '1', CURRENT_TIMESTAMP(), '1', 2);
+insert into reqboards (id, diagnosis, title, need_count, story, reg_date, used_place, req_user_id)values(4, null, '1', 100, '1', CURRENT_TIMESTAMP(), '1', 2);
+insert into reqboards (id, diagnosis, title, need_count, story, reg_date, used_place, req_user_id)values(5, null, '1', 50, '1', CURRENT_TIMESTAMP(), '1', 2);
+insert into reqboards (id, diagnosis, title, need_count, story, reg_date, used_place, req_user_id)values(6, null, '1', 15, '1', CURRENT_TIMESTAMP(), '1', 2);
